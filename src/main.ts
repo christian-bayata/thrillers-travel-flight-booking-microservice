e32263e776b0d41efb0cd9a8e6aab07fc04ff0eb
christian-bayata <franksagie1@gmail.com>
2024-03-30 11:24:39 +0100

    Configure flight booking interceptor logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+import { FlightBookingMsLogger } from './common/logger.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('FlightBookingMicroservice');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL],
+        queue: 'auth_microservice',
+        queueOptions: {
+          durable: true,
+        },
+      },
+    },
+  );
+
+  app.useGlobalInterceptors(new FlightBookingMsLogger());
+  await app
+    .listen()
+    .finally(() =>
+      logger.log(
+        `Flight Booking Microservice: EventBus:${process.env.RABBITMQ_URL}`,
+      ),
+    );
 }
 bootstrap();
