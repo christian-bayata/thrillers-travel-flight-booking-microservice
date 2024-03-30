a399c7378a08f40ac982499cde74ba74793d7560
christian-bayata <franksagie1@gmail.com>
2024-03-30 11:26:31 +0100

    Configure app module with on module init log file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { existsSync, mkdirSync } from 'fs';
+import * as path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('FLIGHT_BOOKING_DATABASE_URL'),
+      }),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  async onModuleInit(): Promise<void> {
+    if (!existsSync(path.join(__dirname, '..', 'logs'))) {
+      mkdirSync(path.join(__dirname, '..', 'logs'));
+    }
+    return;
+  }
+}
